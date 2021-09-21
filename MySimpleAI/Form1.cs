623efa084b33979c01ff5b5ff4cee822d157d785
Anton <65555937+Technonmad@users.noreply.github.com>
2021-09-22 00:28:43 +0300

    Created my first simple AI

---
+++ MySimpleAI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySimpleAI
+{
+    public partial class Form1 : Form
+    {
+        IfTarkov tarkov = new IfTarkov();
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Visible = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tarkov.CheckTarkov(radioButton1.Checked, radioButton5.Checked, radioButton8.Checked, radioButton10.Checked))
+                textBox1.Text = "Вам стоит поиграть в Eskape from Krakov";
+        }
+    }
+}
---
+++ MySimpleAI/IfTarkov.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySimpleAI
+{
+    public class IfTarkov
+    {
+        public bool CheckTarkov(bool a, bool b, bool c, bool d)
+        {
+            if (a && b && c && d)
+                return true;
+            else
+                return false;
+        }
+    }
+}
